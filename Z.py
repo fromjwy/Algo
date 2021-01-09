9ef54b7931ca1fd44970f710efcdc86c87f97fbe
fromjwy <fromjiwony@gmail.com>
2021-01-09 15:24:39 +0900

    backjoon 1074

--- Z.py
+++ Z.py
@@ -7,31 +7,30 @@ def solve(n, x, y):
         result += 1
         if x == X and y+1 == Y:
             print(result)
-            return        
+            return
         result += 1
         if x+1 == X and y == Y:
             print(result)
-            return        
+            return
         result += 1
         if x+1 == X and y+1 == Y:
             print(result)
-            return        
+            return
         result += 1
 
-        
     else:
         half = n//2
-        
-        if x<=X<x+half and y<=Y<y+half:
+
+        if x <= X < x+half and y <= Y < y+half:
             solve(half, x, y)
-        elif x<=X<x+half and y+half<=Y<y+n:
-            result+=half**2
+        elif x <= X < x+half and y+half <= Y < y+n:
+            result += half**2
             solve(half, x, y+half)
-        elif x+half<=X<x+n and y<=Y<y+half:
-            result+=half**2*2
+        elif x+half <= X < x+n and y <= Y < y+half:
+            result += half**2*2
             solve(half, x+half, y)
-        elif x+half<=X<x+n and y+half<=Y<y+n:
-            result+=half**2*3
+        elif x+half <= X < x+n and y+half <= Y < y+n:
+            result += half**2*3
             solve(half, x+half, y+half)
 
 
