859433474ac2670befa271e7a300c34638ef8bb0
fromjwy <fromjiwony@gmail.com>
2021-01-16 16:01:08 +0900

    DFS와 BFS

---
+++ bj1260.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+def DFS(v):
+    visited[v] = True
+    print(v, end=' ')
+
+    for dest in adj[v]:
+        if not visited[dest]:
+            DFS(dest)
+
+
+def BFS(v):
+    q = deque([v])
+    while q:
+        v = q.popleft()
+        if not visited[v]:
+            visited[v] = True
+            print(v, end=' ')
+
+            for dest in adj[v]:
+                if not visited[dest] and dest not in q:
+                    q.append(dest)
+
+
+N, M, V = map(int, input().split())
+adj = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+    adj[v1].append(v2)
+    adj[v2].append(v1)
+
+print(adj)
+
+# 방문가능한 정점이 여러개일 경우 정점번호가 작은 것부터 먼저 방문하기 위해
+for dest in adj:
+    dest.sort()
+
+visited = [False for i in range(N+1)]
+DFS(V)
+print()
+visited = [False for i in range(N+1)]
+BFS(V)
